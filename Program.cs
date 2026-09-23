3db9ff9907ad6eaeb20239cb4408cdad007cdb2c
junior.winkler <juniorgalhardo03@gmail.com>
2026-09-23 16:31:50 -0300

    Initial API

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => new
+{
+    status = "OK",
+    application = "DeployTestApi",
+    version = "1.0.0",
+    message = "API de teste do Deploy Lab"
+});
+
+app.MapGet("/health", () => new
+{
+    status = "Healthy"
+});
+
+app.Run();
